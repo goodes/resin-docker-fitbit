b5681b82bc36bf44e2a7998afc3d6421eb6358d7
Daniel Goodman <daniel@factordata.com>
2016-01-24 09:20:32 +0200

    Checktime now takes time from ON_HOURS env var

--- checktime.py
+++ checktime.py
@@ -3,6 +3,23 @@ import datetime
 import sys
 import os
 
+hours_var = os.getenv("ON_HOURS", "0000-2400")
+
+hour_sets = []
+
+for rng in hours_var.split(':'):
+    try:
+        start, stop = rng.split('-')
+        start = int(start)
+        stop = int(stop)
+        hour_sets.append((start, stop))
+    except Exception as ex:
+        print "Invalid range '{}' in var '{}'".format(
+            rng, hours_var)
+        sys.exit(0)
+
+#print hour_sets
+
 now = datetime.datetime.now()
 
 hour = float(now.hour)
@@ -10,11 +27,13 @@ mins = float(now.minute)
 
 ts = int((hour + mins/100.0) * 100) 
 
+for start, stop in hour_sets:
+    if (start <= ts <= stop):
+        sys.exit(0)
 
-if (730 < ts < 830) or (1130 < ts < 1230):
-    sys.exit(0)
-else:
-    print "Out of range: {} vs {:02d}{:02d}".format(ts, int(hour), int(mins))
-    sys.exit(1)
+print "Not activating, not in range: {:02d}{:02d} not in {}".format(
+        int(hour), int(mins),
+        ", ".join(["{:04d}-{:04d}".format(start,stop) for start,stop in hour_sets]))
+sys.exit(1)
 
 
